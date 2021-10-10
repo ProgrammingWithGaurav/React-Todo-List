2f098cc264a47e3bd8211deae0d5bf4796389bef
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-10-10 12:28:26 +0530

    fixing issues of about component

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
-            This is an about component
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, eum, recusandae modi nihil ratione numquam quam velit odio dicta eos ea non architecto cupiditate facilis quod mollitia inventore ab exercitationem facere ut asperiores. Hic?</p>
+        <div className="container my-3" style={{minHeight: '100vh'}}>
+            <h1>This is an about component</h1>
+            <p>This is a todo-list-app made with react js and bootstrap . You can add many todo here and also delete it. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptates labore officia aspernatur doloribus qui officiis alias quaerat quasi odit ab? Odio dolores doloribus molestias! Magnam aliquam culpa consectetur aspernatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias saepe iure sit. Numquam ea voluptate fugit amet harum placeat obcaecati, nam porro dolor ullam error est culpa aspernatur, voluptas ex!</p>
         </div>
     )
 }
